8a20b9448c25f79a37f3193bb6cae775b1b7cc9a
amirmammadov <amirmammado@gmail.com>
2024-09-28 19:40:36 +0400

    standalone was replaced with module

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,14 +1,10 @@
 import { Component, signal, output } from '@angular/core';
 
-import { FormsModule } from '@angular/forms';
-
 import type { IInvestmentData } from '../investment.model';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
 })
